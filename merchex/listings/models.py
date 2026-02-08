34ae03cfd5580f125467c21ee82b2f36e7e2f011
de Breyne <111784301+helio75011@users.noreply.github.com>
2026-02-09 00:55:03 +0100

    update admin django

--- merchex/listings/models.py
+++ merchex/listings/models.py
@@ -2,11 +2,21 @@ from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 class Band(models.Model):
+
+    class Genre(models.TextChoices):
+        HIP_HOP = 'HH'
+        SYNTH_POP = 'SP'
+        ALTERNATIVE_ROCK = 'AR'
+
     name = models.fields.CharField(max_length=100)
-    genre = models.fields.CharField(max_length=50)
+    genre = models.fields.CharField(choices=Genre.choices, max_length=5)
     biography = models.fields.CharField(max_length=1000)
     year_formed = models.fields.IntegerField(
     validators=[MinValueValidator(1900), MaxValueValidator(2021)]
     )
     active = models.fields.BooleanField(default=True)
     official_homepage = models.fields.URLField(null=True, blank=True)
+
+    # modifier le rendu des noms d'objets dans l'admin Django en string
+    def __str__(self):  
+        return f'{self.name}'
