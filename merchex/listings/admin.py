f34864cab5aa9a7559b0e7767b64def052d09bbd
de Breyne <111784301+helio75011@users.noreply.github.com>
2026-02-09 14:06:33 +0100

    add model list

--- merchex/listings/admin.py
+++ merchex/listings/admin.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
-from listings.models import Band
+from listings.models import Band, Listing
 
-class BandAdmin(admin.ModelAdmin):  # nous insérons ces deux lignes..
-    list_display = ('name', 'year_formed', 'genre') # liste les champs que nous voulons sur l'affichage de la liste
-
-admin.site.register(Band, BandAdmin) # nous modifions cette ligne, en ajoutant un deuxième argument
+admin.site.register(Band) 
+admin.site.register(Listing) 
