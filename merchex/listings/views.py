e36988643b30bb1a6f1a60cdc736e8882be3f24a
de Breyne <111784301+helio75011@users.noreply.github.com>
2026-02-10 22:51:20 +0100

    add contact form

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,18 +1,35 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from listings.models import Band
+from listings.forms import ContactUsForm
+
+def contact(request):
+
+        if request.method == 'POST':
+                # créer une instance de notre formulaire et le remplir avec les données POST
+                form = ContactUsForm(request.POST)
+        else:
+                # ceci doit être une requête GET, donc créer un formulaire vide
+                form = ContactUsForm()  # ajout d’un nouveau formulaire ici
+        
+        return render(request,
+                'listings/contact.html',
+                {'form': form})  # passe ce formulaire au gabarit
+
 
 def band_list(request):  # renommer la fonction de vue
-   bands = Band.objects.all()
-   return render(request,
-           'listings/band_list.html',  # pointe vers le nouveau nom de modèle
-           {'bands': bands})
+        bands = Band.objects.all()
+        return render(request,
+                'listings/band_list.html',  # pointe vers le nouveau nom de modèle
+                {'bands': bands})
+
 
 def band_detail(request, id):
-  band = Band.objects.get(id=id)  # nous insérons cette ligne pour obtenir le Band avec cet id
-  return render(request,
-          'listings/band_detail.html',
-          {'band': band}) # nous mettons à jour cette ligne pour passer le groupe au gabarit
+        band = Band.objects.get(id=id)  # nous insérons cette ligne pour obtenir le Band avec cet id
+        return render(request,
+                'listings/band_detail.html',
+                {'band': band}) # nous mettons à jour cette ligne pour passer le groupe au gabarit
+
 
 def about(request):
-    return HttpResponse('<h1>À propos</h1> <p>Nous adorons merch !</p>')
+        return HttpResponse('<h1>À propos</h1> <p>Nous adorons merch !</p>')
